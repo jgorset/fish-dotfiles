e8363fa17d2cc5e1fae7ed3ad7222053359671c6
jgorset <jgorset@gmail.com>
2019-05-06 16:17:32 +0200

    Fix colors

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,8 +1,13 @@
 # Base16 Shell
 if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    set BASE16_SHELL "$HOME/.config/base16-shell"
     source "$BASE16_SHELL/profile_helper.fish"
 end
 
 # Don't greet
 set fish_greeting
+
+set fish_color_command blue --bold
+set fish_color_autosuggestion white
+set fish_color_param blue
+set fish_color_error red
