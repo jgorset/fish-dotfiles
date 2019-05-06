25b1b3c042b99006e1198b17b72b9ddaeb065e69
jgorset <jgorset@gmail.com>
2019-05-06 17:17:12 +0200

    Add more colors

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -7,7 +7,12 @@ end
 # Don't greet
 set fish_greeting
 
+set fish_color_normal white
 set fish_color_command blue --bold
 set fish_color_autosuggestion white
-set fish_color_param blue
+set fish_color_param cyan
 set fish_color_error red
+set fish_color_quote cyan
+set fish_color_end white
+set fish_color_redirection white
+set fish_color_operator red
